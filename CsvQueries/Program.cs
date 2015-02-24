1e414203ad1d315afcde1ba09e8b24ff0fd13a2f
DianaK <diana.kosashka@gmail.com>
2015-02-24 23:56:50 +0200

    Queries

---
+++ CsvQueries/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CsvQueries
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> columnNames;
+            var csvFileHash = ReadFile(out columnNames);
+            var queryExecutor = new QueryExecutor(csvFileHash, columnNames);
+            Console.Write(queryExecutor.Select(new List<string>() {"id", "name"}, 5));
+            Console.WriteLine();
+            Console.Write(queryExecutor.Sum("id"));
+            Console.WriteLine();
+            Console.Write(queryExecutor.Show());
+            Console.WriteLine();
+            Console.Write(queryExecutor.Find("-"));
+        }
+
+        public static Dictionary<int, IDictionary<string, string>> ReadFile(out List<string> columnNames)
+        {
+            var csvFileHash = new Dictionary<int, IDictionary<string, string>>();// for every row, hash with column names and column values
+            string[] keys = {}; // column names
+            int lineNumber = 0;
+            using (var file =
+                new StreamReader(@"C:\Test\AFTPPath\csvFile.txt"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (lineNumber == 0)
+                    {
+                        keys = line.Split(',');
+                        lineNumber++;
+                        continue;
+                    }
+                    csvFileHash.Add(lineNumber, new Dictionary<string, string>());
+                    string[] currentRowValues = line.Split(','); // column values
+                    for (int i = 0; i < keys.Count(); i++)
+                    {
+                        string value;
+                        if (i >= currentRowValues.Count())
+                        {
+                            value = String.Empty;
+                            // if the column names are more than column values ( the csv file is not correct)
+                        }
+                        else
+                        {
+                            value = currentRowValues[i];
+                        }
+                        csvFileHash[lineNumber].Add(keys[i], value);
+                    }
+                    lineNumber++;
+                }
+            }
+            columnNames = keys.ToList();
+            return csvFileHash;
+        }
+
+        
+    }
+}
---
+++ CsvQueries/QueryExecutor.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsvQueries
+{
+    public class QueryExecutor
+    {
+        private readonly Dictionary<int, IDictionary<string, string>> csvFileHash;
+        private readonly List<string> columnNames;
+
+        public QueryExecutor(Dictionary<int, IDictionary<string, string>> csvFileHash, List<string> columnNames)
+        {
+            this.csvFileHash = csvFileHash;
+            this.columnNames = columnNames;
+        }
+
+        public string Select(List<string> columns, int limit = Int32.MaxValue)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            foreach (var columnNumber in csvFileHash.Keys.Where(p=>p < limit))
+            {
+                var currentRow = csvFileHash[columnNumber];
+                foreach (var columnName in columns)
+                {
+                    strBuilder.AppendFormat("{0}|", currentRow[columnName]);
+                }
+                strBuilder.AppendLine();
+            }
+            return strBuilder.ToString();
+        }
+
+        public string Sum(string column)
+        {
+            int sum = csvFileHash.Keys.Sum(columnNumber => Convert.ToInt32(csvFileHash[columnNumber][column]));
+            return sum.ToString();
+        }
+
+        public string Show()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            foreach (var columnName in columnNames)
+            {
+                strBuilder.AppendFormat("{0},", columnName);
+            }
+            return strBuilder.ToString(0, strBuilder.Length - 2); // lenght - 2 cuts the last comma
+        }
+
+        public string Find(string x)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            foreach (var columnNumber in csvFileHash.Keys)
+            {
+                var currentRow = csvFileHash[columnNumber];
+                foreach (var columnValue in currentRow.Values)
+                {
+                    if (columnValue.Contains(x))
+                    {
+                        foreach (var key in currentRow.Keys)
+                        {
+                            strBuilder.AppendFormat("{0}|", currentRow[key]);
+                        }
+                        strBuilder.AppendLine();
+                        break;
+                    }
+                }
+            }
+            return strBuilder.ToString();
+        }
+    }
+}
